ba2140c0e6c1f6b775729e4a790e58ce7f387752
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2023-03-05 18:42:05 +0000

    added split lines loader function

--- 2015/aoc.py
+++ 2015/aoc.py
@@ -10,3 +10,7 @@ def load_lines(filename: str) -> Iterator[str]:
     with open(filename) as file:
         for line in file:
             yield line.strip()
+
+
+def load_split_lines(filename: str, token: str, dtype=str) -> list[tuple]:
+    return [tuple(map(dtype, line.split(token))) for line in load_lines(filename)]
