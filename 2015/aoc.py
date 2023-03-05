372244ac4bb4f5a75f053393912887c031344dc2
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2023-03-05 18:26:40 +0000

    2015 day 2

--- 2015/aoc.py
+++ 2015/aoc.py
@@ -1,5 +1,12 @@
-# Data Loading Methods
+from typing import Iterator
+
 
 def load_single_line(filename: str) -> str:
     with open(filename) as file:
         return next(file).strip()
+
+
+def load_lines(filename: str) -> Iterator[str]:
+    with open(filename) as file:
+        for line in file:
+            yield line.strip()
